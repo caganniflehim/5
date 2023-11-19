661a9afe7ee983d71cbb6a578b1977ec6c4a9c37
caganniflehim <omercagand@gmail.com>
2023-11-19 19:14:46 +0300

    Initial commit

---
+++ 5/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace _5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonGoster = new System.Windows.Forms.Button();
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.textBoxSoyad = new System.Windows.Forms.TextBox();
+            this.radioButtonKadin = new System.Windows.Forms.RadioButton();
+            this.radioButtonErkek = new System.Windows.Forms.RadioButton();
+            this.comboBoxSehirler = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePickerDogumTarihi);
+            this.groupBox1.Controls.Add(this.comboBoxSehirler);
+            this.groupBox1.Controls.Add(this.radioButtonErkek);
+            this.groupBox1.Controls.Add(this.radioButtonKadin);
+            this.groupBox1.Controls.Add(this.textBoxSoyad);
+            this.groupBox1.Controls.Add(this.textBoxAd);
+            this.groupBox1.Controls.Add(this.buttonGoster);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(35, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(627, 364);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgiler";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(25, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(25, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(25, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Cinsiyet:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(25, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Doğum Yeri:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(25, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Doğum Tarihi:";
+            // 
+            // buttonGoster
+            // 
+            this.buttonGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.buttonGoster.Location = new System.Drawing.Point(238, 280);
+            this.buttonGoster.Name = "buttonGoster";
+            this.buttonGoster.Size = new System.Drawing.Size(187, 59);
+            this.buttonGoster.TabIndex = 5;
+            this.buttonGoster.Text = "Bilgileri Göster";
+            this.buttonGoster.UseVisualStyleBackColor = true;
+            this.buttonGoster.Click += new System.EventHandler(this.buttonGoster_Click);
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.Location = new System.Drawing.Point(183, 26);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(242, 26);
+            this.textBoxAd.TabIndex = 6;
+            // 
+            // textBoxSoyad
+            // 
+            this.textBoxSoyad.Location = new System.Drawing.Point(183, 77);
+            this.textBoxSoyad.Name = "textBoxSoyad";
+            this.textBoxSoyad.Size = new System.Drawing.Size(242, 26);
+            this.textBoxSoyad.TabIndex = 7;
+            // 
+            // radioButtonKadin
+            // 
+            this.radioButtonKadin.AutoSize = true;
+            this.radioButtonKadin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButtonKadin.Location = new System.Drawing.Point(183, 123);
+            this.radioButtonKadin.Name = "radioButtonKadin";
+            this.radioButtonKadin.Size = new System.Drawing.Size(93, 29);
+            this.radioButtonKadin.TabIndex = 8;
+            this.radioButtonKadin.TabStop = true;
+            this.radioButtonKadin.Text = "Kadın";
+            this.radioButtonKadin.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonErkek
+            // 
+            this.radioButtonErkek.AutoSize = true;
+            this.radioButtonErkek.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButtonErkek.Location = new System.Drawing.Point(298, 123);
+            this.radioButtonErkek.Name = "radioButtonErkek";
+            this.radioButtonErkek.Size = new System.Drawing.Size(92, 29);
+            this.radioButtonErkek.TabIndex = 9;
+            this.radioButtonErkek.TabStop = true;
+            this.radioButtonErkek.Text = "Erkek";
+            this.radioButtonErkek.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxSehirler
+            // 
+            this.comboBoxSehirler.AutoCompleteCustomSource.AddRange(new string[] {
+            "İstanbul",
+            "Ankara",
+            "Adana",
+            "Bursa",
+            "İzmir"});
+            this.comboBoxSehirler.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBoxSehirler.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.comboBoxSehirler.FormattingEnabled = true;
+            this.comboBoxSehirler.Items.AddRange(new object[] {
+            "İstanbul",
+            "Ankara",
+            "Adana",
+            "Bursa",
+            "İzmir"});
+            this.comboBoxSehirler.Location = new System.Drawing.Point(183, 171);
+            this.comboBoxSehirler.Name = "comboBoxSehirler";
+            this.comboBoxSehirler.Size = new System.Drawing.Size(242, 28);
+            this.comboBoxSehirler.TabIndex = 10;
+            this.comboBoxSehirler.SelectedIndexChanged += new System.EventHandler(this.comboBoxSehirler_SelectedIndexChanged);
+            // 
+            // dateTimePickerDogumTarihi
+            // 
+            this.dateTimePickerDogumTarihi.Location = new System.Drawing.Point(183, 215);
+            this.dateTimePickerDogumTarihi.Name = "dateTimePickerDogumTarihi";
+            this.dateTimePickerDogumTarihi.Size = new System.Drawing.Size(242, 26);
+            this.dateTimePickerDogumTarihi.TabIndex = 11;
+            this.dateTimePickerDogumTarihi.Value = new System.DateTime(2022, 1, 1, 0, 0, 0, 0);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerDogumTarihi;
+        private System.Windows.Forms.ComboBox comboBoxSehirler;
+        private System.Windows.Forms.RadioButton radioButtonErkek;
+        private System.Windows.Forms.RadioButton radioButtonKadin;
+        private System.Windows.Forms.TextBox textBoxSoyad;
+        private System.Windows.Forms.TextBox textBoxAd;
+        private System.Windows.Forms.Button buttonGoster;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 5/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        string dogumYeri = "";
+
+        private void comboBoxSehirler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dogumYeri = comboBoxSehirler.SelectedItem.ToString();
+
+        }
+
+        private void buttonGoster_Click(object sender, EventArgs e)
+        {
+            string cinsiyet = "";
+            if (radioButtonErkek.Checked)
+                cinsiyet = "Erkek";
+            else if (radioButtonKadin.Checked)
+                cinsiyet = "Kadın";
+            else
+                cinsiyet = "Belirtilmedi";
+
+            string dogumTarihi = dateTimePickerDogumTarihi.Text;
+            string bilgiler = "Adı:\t" + textBoxAd.Text
+                + "\nSoyadı:\t" + textBoxSoyad.Text
+                + "\nCinsiyet:\t" + cinsiyet
+                + "\nD.Yeri:\t" + dogumYeri
+                + "\nD.Tarihi:\t" + dogumTarihi;
+
+            textBoxAd.Text = "";
+            textBoxSoyad.Text = "";
+            textBoxAd.Select();
+            radioButtonErkek.Checked = false;
+            radioButtonKadin.Checked = false;
+            comboBoxSehirler.SelectedIndex = 0;
+            dateTimePickerDogumTarihi.Value = new DateTime(2000, 01, 01);
+
+
+            MessageBox.Show(bilgiler);
+        }
+
+    }
+}
